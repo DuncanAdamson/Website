99a044a870273d625c8cf9f21499d3eaaaf77bcd
Delagon <duncan.adamson@liverpool.ac.uk>
2021-06-21 12:26:01 +0100

    chaged the updater

--- web_update.sh
+++ web_update.sh
@@ -3,4 +3,5 @@ rm public_html -r -f
 wget https://github.com/Delagon/Website/archive/refs/heads/main.zip
 unzip main.zip
 mv Website-main public_html
+mv public_html/web_update.sh web_update.sh
 at now + 1 hour <<< "sh web_update.sh"
