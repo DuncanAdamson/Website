7f335e2c6755f0bf93a2f1c3ac45253034d154ad
Duncan Adamson <duncan.adamson@liverpool.ac.uk>
2021-06-23 13:40:26 +0100

    Update web_update.sh

--- web_update.sh
+++ web_update.sh
@@ -1,6 +1,6 @@
 rm main.zip
 rm public_html -r -f
-wget https://github.com/Delagon/Website/archive/refs/heads/main.zip
+wget https://github.com/DuncanAdamson/Website/archive/refs/heads/main.zip
 unzip main.zip
 mv Website-main public_html
 mv public_html/web_update.sh web_update.sh
